0cb44f068b3add03252e0a94af166f31bdd30e62
Jonathan Looney <design@jonathanlooney.com>
2017-11-29 15:15:32 -0600

    dates

--- src/js/app.js
+++ src/js/app.js
@@ -5,13 +5,33 @@ import 'smoothstate';
 
 
 
-$(function() {
-    var d =new Date();
-    var curmonth = d.getMonth()+1;
-    var curDate = d.getFullYear()+"-"+curmonth+"-"+d.getDate();
+//$(function() {
+ //   var d =new Date();
+  //  var curmonth = d.getMonth()+1;
+   // var curDate = d.getFullYear()+"-"+curmonth+"-"+d.getDate();
 
-    $(".grid-item[data-date="+curDate+"]").append('<div id="date-featured"></div>').find('#date-featured').addClass("today");
+ //    $(".grid-item[data-date="+curDate+"]").append('<div id="date-featured"></div>').find('#date-featured').addClass("today");
+//});
+
+
+
+
+$(function() {
+  var date = new Date(),
+    currentDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+  $(".grid-item").each(function() {
+    var specifiedDate = $(this).data('date');
+    if (specifiedDate == currentDate) {
+      $(this).append('<div id="date-featured"></div>').find('#date-featured').addClass("today");
+    } else if (currentDate > specifiedDate) {
+      $(this).addClass('past');
+    } else {
+      $(this).addClass('future');
+    }
+  });
 });
+
+
 
 
 
@@ -38,17 +58,17 @@ $(function() {
 
 
 
-TweenMax.to(".star__logo", 1, {  width: "8rem", transform:"translateY(4vh)",delay:2,ease:Expo.easeIn});
+TweenMax.to(".star__logo", 1, {  width: "8rem", transform:"translateY(4vh)",delay:1,ease:Expo.easeIn});
 
-TweenMax.to(".star__burst", .85, {  opacity:"1",delay:2.5,ease:Expo.easeIn});
-TweenMax.to(".ground--back", 1, {  fill:"#272067",delay:2,ease:Expo.easeIn});
-TweenMax.to(".ground--middle", 1, {  fill:"#1E1759",delay:2,ease:Expo.easeIn});
-TweenMax.to(".home", .85, {  backgroundColor:"#1A144F",delay:2.5,ease:Expo.easeIn});
+TweenMax.to(".star__burst", .85, {  opacity:"1",delay:1.5,ease:Expo.easeIn});
+TweenMax.to(".ground--back", 1, {  fill:"#272067",delay:1,ease:Expo.easeIn});
+TweenMax.to(".ground--middle", 1, {  fill:"#1E1759",delay:1,ease:Expo.easeIn});
+TweenMax.to(".home", .85, {  backgroundColor:"#1A144F",delay:1.5,ease:Expo.easeIn});
 
 
 
-TweenMax.to(".banner__wordmark", .5, {  transform:"translateY(0vh)" ,opacity:"0",delay:2,ease:Expo.easeIn});
-TweenMax.to(".stars", .75, {  opacity:"1",delay:2.5,ease:Expo.easeIn});
+TweenMax.to(".banner__wordmark", .5, {  transform:"translateY(0vh)" ,opacity:"0",delay:1,ease:Expo.easeIn});
+TweenMax.to(".stars", .75, {  opacity:"1",delay:1.5,ease:Expo.easeIn});
 //TweenMax.to(".banner__ground", .75, {  opacity:"1",delay:4.25,ease:Expo.easeIn});
 //TweenMax.to(".banner__background", .75, {  opacity:"1",delay:3.5,ease:Expo.easeIn});
 
